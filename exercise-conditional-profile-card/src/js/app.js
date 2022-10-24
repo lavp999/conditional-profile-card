3533f4f43fc4b3fdf086515932bbd23bd35845b6
Luis <lavp99@hotmail.com>
2022-10-24 21:25:14 +0200

    terminado pero no para presentar

--- exercise-conditional-profile-card/src/js/app.js
+++ exercise-conditional-profile-card/src/js/app.js
@@ -26,21 +26,50 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  let cover = null;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  else cover = `<div class="cover"><img src="${variables.background}" /></div>`;
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  document.querySelector("#widget_content").innerHTML =
+    `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>` +
+    (variables.name == null
+      ? "Desconocido"
+      : variables.name +
+        " " +
+        (variables.lastname === null ? "" : variables.lastname)) +
+    `</h1>
+          <h2>` +
+    (variables.role === null ? "" : variables.role) +
+    `</h2>
+          <h3>` +
+    (variables.city === null ? "" : variables.city + ",") +
+    (variables.country === null ? "" : variables.country) +
+    `</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="` +
+    (variables.twitter === null
+      ? "#"
+      : "https://twitter.com/4geeksacademy/" + variables.twitter) +
+    `"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="` +
+    (variables.github === null
+      ? "#"
+      : "https://github.com/4geeksacademy/" + variables.github) +
+    `"><i class="fab fa-github"></i></a></li>
+            <li><a href="` +
+    (variables.linkedin === null
+      ? "#"
+      : "https://linkedin.com/4geeksacademy/" + variables.linkedin) +
+    `"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="` +
+    (variables.instagram === null
+      ? "#"
+      : "https://instagram.com/4geeksacademy/" + variables.instagram) +
+    `"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -61,7 +90,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
